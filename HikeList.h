a9c959364b2638f0c43ae696db43f1629faed2fb
Tim Tran <>
2024-12-02 20:50:31 -0800

    lint fixes

--- HikeList.h
+++ HikeList.h
@@ -14,28 +14,27 @@
 #ifndef HIKE_LIST_H
 #define HIKE_LIST_H
 
+#include "Hike.h"
+
 #include <iostream>
 #include <map>
 #include <string>
 
-#include "Hike.h"
-
 class HikeList
 {
 public:
-  void addHike(const Hike &hike, double price);
-  void addHike(const std::string &location,
-               const std::string &hikeName, int duration,
-               char difficulty, double price);
+  void addHike(const Hike& hike, double price);
+  void addHike(const std::string& location, const std::string& hikeName,
+    int duration, char difficulty, double price);
 
-  double getPrice(const std::string &hikeName) const;
+  double getPrice(const std::string& hikeName) const;
   void printAllLocations() const;
-  void printByLocation(const std::string &location) const;
+  void printByLocation(const std::string& location) const;
   void printByDuration() const;
   void printByDuration(int days) const;
   void printByDifficulty(char difficulty) const;
   void printByPrice() const;
-  void printByHikeName(const std::string &hikeName) const;
+  void printByHikeName(const std::string& hikeName) const;
 
   void clearList();
 
